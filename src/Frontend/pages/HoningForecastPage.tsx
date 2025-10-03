2f89f7ab7e5d7b66537ebc2adcf3553944560695
Kenivia <47839308+Kenivia@users.noreply.github.com>
2025-10-04 03:31:27 +1300

    added a footer for the github link

--- src/Frontend/pages/HoningForecastPage.tsx
+++ src/Frontend/pages/HoningForecastPage.tsx
@@ -3,8 +3,10 @@ import HoningForecastUI from '../features/honing_forecast/HoningForecastUI.tsx'
 
 export default function HoningForecastPage() {
     return (
-
-        <HoningForecastUI />
+        <div>
+            <HoningForecastUI />
+            <footer style={{ textAlign: "right" }}> <a href="https://github.com/Kenivia/Honing-Forecast"> Github</a></footer>
+        </div >
 
 
     )
